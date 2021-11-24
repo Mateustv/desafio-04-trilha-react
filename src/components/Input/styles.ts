5c0d18f0bf3a7825d401ad2fb1c87931a77d61be
Mateus T <mateustavarestv@hotmail.com>
2021-11-24 18:29:11 -0300

    passando o projeto todo para typescript

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface StylesInput {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<StylesInput>`
   display: flex;
   align-items: center;
 
